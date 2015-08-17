a64c417b9a0f9e6815e8ae121cb7e264c59fa1b8
jophinjohn <jophinjohn@hotmail.com>
2015-08-17 12:31:24 +0000

    update

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,15 +15,16 @@ require 'faker'
    )
  end
  posts = Post.all
- 
+
  # Create Comments
  100.times do
    Comment.create!(
      post: posts.sample,
      body: Faker::Lorem.paragraph
    )
+   
  end
- 
+   
  puts "Seed finished"
  puts "#{Post.count} posts created"
  puts "#{Comment.count} comments created"
