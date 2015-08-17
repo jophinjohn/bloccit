ca56e43a759f3dbebb9f16b9d6dc89984694016f
jophinjohn <jophinjohn@hotmail.com>
2015-08-17 12:12:13 +0000

    Assignment 32 complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,6 +15,7 @@ require 'faker'
    )
  end
  posts = Post.all
+ comments = Comment.all
  
  # Create Comments
  100.times do
@@ -23,7 +24,23 @@ require 'faker'
      body: Faker::Lorem.paragraph
    )
  end
+ if posts.any?{|item| item[:title]=="Jophin's post" && item[:body] == "Hello Jophin. This is your first post"}
+   puts "Cannot create unique post"
+ else
+   checkpost = Post.create!(
+     title: "Jophin's post",
+     body: "Hello Jophin. This is your first post"
+   )
+ end 
  
+ if comments.any?{|item| item[:post_id]== checkpost && item[:body] == "Hello Jophin. This is your first comment"}
+   puts "Cannot create unique comment"
+ else
+   Comment.create!(
+    post: checkpost,
+    body: "Hello Jophin. This is your first comment"
+   ) 
+ end
  puts "Seed finished"
  puts "#{Post.count} posts created"
  puts "#{Comment.count} comments created"
