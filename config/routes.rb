1026e18bcbc46166391552ce10bb6931d2edada6
jophinjohn <jophinjohn@hotmail.com>
2015-08-25 10:41:34 +0000

    Assignment 33 complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
+ 
    resources :posts
+   resources :advertisements
    get 'about' => 'welcome#about'
+   
   
    root to: 'welcome#index'
+   
+end
+  
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -57,4 +65,3 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
