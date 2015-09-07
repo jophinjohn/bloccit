681c032bccbedd238aa0d338658031ea0e518baa
jophinjohn <jophinjohn@hotmail.com>
2015-09-07 00:11:55 +0000

    Assignment 38 complete

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,12 +1,16 @@
 class Post < ActiveRecord::Base
     has_many :comments
     belongs_to :user
-    default_scope { order('created_at DESC') }
+    #default_scope { order('created_at DESC') }
+    scope :ordered_by_title, -> { order('title DESC' )}
+    scope :ordered_by_reverse_created_at,  ->(time) { where("created_at < ?", time) }
+    
 end
 
 
 # == Schema Information
-#
+#exit
+
 # Table name: posts
 #
 #  id         :integer          not null, primary key
