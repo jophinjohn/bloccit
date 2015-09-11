f18e5264359b4d051505ad5214c43058aea67e74
jophinjohn <jophinjohn@hotmail.com>
2015-09-11 21:17:11 +0000

    Assignment 39

--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -1,5 +1,20 @@
 class PostPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      if user.present?
+        if user.admin? || user.moderator?
+          scope.all
+        else  
+          scope.where(:user_id => user.id)
+        end
+      else
+          scope.where(:user_id => nil)
+      end
+    end
+  end
+  
   def index?
     true
   end
+  
 end
