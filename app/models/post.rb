e9f0ab426fd3c45cc4b68139c00189a188fba557
jophinjohn <jophinjohn@hotmail.com>
2015-09-12 20:24:08 +0000

    Assignment 38 unscoped

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@ class Post < ActiveRecord::Base
     belongs_to :user
     default_scope { order('created_at DESC') }
     scope :ordered_by_title, -> { order('title DESC' ) }
-    scope :ordered_by_reverse_created_at,  -> { order('created_at ASC') }
+    scope :ordered_by_reverse_created_at,  -> {unscoped.order('created_at ASC') }
     
 end
 
