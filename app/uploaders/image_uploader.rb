14510397a94104deb0c7e08ef74aeebb0fe70e62
jophinjohn <jophinjohn@hotmail.com>
2015-10-04 21:04:52 +0000

    Complete Assignment 48 (3)

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -25,8 +25,8 @@ class ImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  process :scale => [200, 300]
-  #
+  #process :scale => [200, 300]
+  process :resize_to_fill => [200, 300]
   # def scale(width, height)
   #   # do something
   # end
