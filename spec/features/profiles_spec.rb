3ecc0ea31ab4a8e6b09ae143d81edce36bbec0e5
jophinjohn <jophinjohn@hotmail.com>
2015-11-30 02:36:04 +0000

    Assignment 54 complete

--- spec/features/profiles_spec.rb
+++ spec/features/profiles_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 describe "Visiting profiles" do
 
   include TestFactories
+  include Warden::Test::Helpers
+  Warden.test_mode!
 
   before do
     @user = authenticated_user
@@ -22,6 +24,20 @@ describe "Visiting profiles" do
        expect( page ).to have_content(@post.title)
        expect( page ).to have_content(@comment.body)
     end
-
+  describe "signed in"  do
+    before do
+      login_as(@user, scope: :user)
+    end
+    it "shows profile" do 
+      visit user_path(@user)
+      expect(current_path).to eq(user_path(@user))
+      
+      expect( page ).to have_content(@user.name)
+      expect( page ).to have_content(@post.title)
+      expect( page ).to have_content(@comment.body)
+    end
+    after { Warden.test_reset! }
+  end
+      
   end
 end
