6ccc2fca54dadc915b6d4230defbfdc5fa30d656
jophinjohn <jophinjohn@hotmail.com>
2015-11-30 00:50:59 +0000

    Checkpoint54-publicprofiles complete

--- app/models/post.rb
+++ app/models/post.rb
@@ -25,17 +25,18 @@ class Post < ActiveRecord::Base
      update_attribute(:rank, new_rank)
     end
     
-    def create_vote
-      user.votes.create(value: 1, post: self)
-    end
-    default_scope { order('rank DESC') }
-  
+    
+    scope :visible_to, -> (user) { user ? all : joins(:topic).where('topics.public' => true) }
     
     validates :title, length: { minimum: 5 }, presence: true
     validates :body, length: { minimum: 20 }, presence: true
     validates :topic, presence: true
     validates :user, presence: true
+    def create_vote
+      user.votes.create(value: 1, post: self)
+    end
 end
+
 
 
 # == Schema Information
