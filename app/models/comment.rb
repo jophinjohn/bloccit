faab47a7cd834eea00946a167f0c82bd1f8449c8
jophinjohn <jophinjohn@hotmail.com>
2015-12-30 09:55:07 +0000

    Assignment complete

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -5,7 +5,7 @@ class Comment < ActiveRecord::Base
   validates :post, presence: true
   validates :user, presence: true
   after_create :send_favorite_emails
-  default_scope { order('updated_at DESC') }
+  default_scope { order('updated_at ASC') }
    private
  
    def send_favorite_emails
