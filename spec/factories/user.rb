0b4d1656627d16004e08f866ff1ed8a6a2530fa4
jophinjohn <jophinjohn@hotmail.com>
2015-12-30 02:50:09 +0000

    Indentation changes

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -6,14 +6,14 @@ FactoryGirl.define do
      password_confirmation "helloworld"
      confirmed_at Time.now
    
-   factory :user_with_post_and_comment do
+     factory :user_with_post_and_comment do
        after(:build) do |user|
         post = create(:post, user: user)
         create(:comment, user: user, post: post)
    
        end
-  end
-  end
+     end
+    end
 end
  
  
